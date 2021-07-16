252860c30da77b812a79a7423ca2d33e99f3494e
lancelee2885 <lancelee2885@gmail.com>
2021-07-16 12:32:37 -0700

    step6 partially completed

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
+
 @app.route("/")
 def home_page():
     """Render a page about pets"""
@@ -35,18 +36,38 @@ def home_page():
 
     return render_template("home.html", pets=pets)
 
-@app.route('/add', methods=["GET","POST"])
+
+@app.route('/add', methods=["GET", "POST"])
 def show_form():
     """Show user form and update pet list on database"""
+
     form = PetForm()
 
-    # Validate data
-    if(form.validate_on_submit()):
+
+    if form.validate_on_submit():
         name = form.name.data
         species = form.species.data
         photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        
+        pet = Pet(name=name, 
+                  species=species, 
+                  photo_url=photo_url, 
+                  age=age, 
+                  notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+
         return redirect('/')
 
-        
-    return render_template('/add', form=form)
+    return render_template('add_pet.html', form=form)
 
+@app.route('/<int:pet_id>')
+def render_pet_page(pet_id):
+    """Show pet information"""
+
+    pet = Pet.query.get(pet_id)
+
+    return render_template('pet_info.html', pet=pet)
--- forms.py
+++ forms.py
@@ -3,13 +3,20 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
 from wtforms import StringField, BooleanField, SelectField
-from wtforms.validators import InputRequired, Optional, URL
+from wtforms.validators import InputRequired, Optional, URL, AnyOf
 from wtforms.widgets.core import Option
+
 
 class PetForm(FlaskForm):
 
-	name = StringField("Pet Name", validators=[InputRequired()])
-	species = StringField("Species", validators=[InputRequired()])
-	photo_url = StringField("Photo URL", validators=[Optional(), URL()])
-	age = SelectField("Age", choices=[()],validators=[InputRequired()])  # TODO: add values for choices (data, things_dislayed)
-	notes = StringField("Notes", validators=[Optional()])
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = StringField("Species",
+                          validators=[InputRequired(),
+                                      AnyOf(values=["cat", "dog", "porcupine", "Cat", "Dog", "Porcupine", ], message="Must be cat, dog, or porcupine")])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    age = SelectField(
+        "Age",
+        choices=[('baby', 'Baby'), ('young', 'Young'),
+                 ('adult', 'Adult'), ('senior', 'Senior')],
+        validators=[InputRequired()])
+    notes = StringField("Notes", validators=[Optional()])
